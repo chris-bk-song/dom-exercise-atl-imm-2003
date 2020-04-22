3c54ec141f0c737f2f0949e134da12f9a33844ec
Chris Song <chris.bk.song@gmail.com>
2020-04-22 11:49:18 -0400

    creates clickable anchor elements in the DOM

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,33 @@
-
-
 const addresses = [
     "https://google.com",
     "https://bing.com",
-    "https://duckduckgo.com"
+    "https://duckduckgo.com",
+    "https://yahoo.com"
+
 ];
 
+const navLinks = document.createElement('ul');
+const navContainer = document.querySelector('nav.js-link-container');
+
+navContainer.appendChild(navLinks);
+
+// ONE WAY
+function createLink(address) {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.setAttribute('href', address);
+    link.textContent = address;
+    listItem.appendChild(link);
+    navLinks.appendChild(listItem);
+}
+addresses.forEach(createLink);
+
+// ANOTHER WAY
+// addresses.forEach(function (address) {
+//     const listItem = document.createElement('li');
+//     const link = document.createElement('a');
+//     link.setAttribute('href', address);
+//     link.textContent = address;
+//     listItem.appendChild(link);
+//     navLinks.appendChild(listItem);
+// })
